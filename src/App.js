a788f61c8465325301923eb0e6f985da70866d57
Nikki Daye <nikkidaye@gmail.com>
2019-07-27 15:28:37 -0400

    updated SignIn component and App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Contact from "./components/Contact";
+import MainCategories from "./components/MainCategories";
+import NewUser from "./components/NewUser";
+import SignIn from "./components/SignIn";
+import SingleCategory from "./components/SingleCategory";
+import UserProfile from "./components/UserProfile";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      email: null
+    };
+  }
+
+  componentDidMount = () => {
+    this.getUser();
+  };
+
+  updateUser = userObject => {
+    this.setState(userObject);
+  };
+
+  getUser = () => {
+    axios.get("/user/").then(res => {
+      console.log("Get User response: ");
+      console.log(res.data);
+      if (res.data.user) {
+        console.log("Get User: There is a user saved in server: ");
+        this.setState({
+          loggedIn: true,
+          email: res.data.user.email
+        });
+      } else {
+        console.log("Get User: no user");
+        this.setState({
+          loggedIn: false,
+          email: null
+        });
+      }
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        {/* <Contact />
+        <MainCategories />
+        <NewUser />
+        <SignIn />
+        <SingleCategory />
+        <UserProfile /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,85 @@
-import React { Component } from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 class SignIn extends React.Component {
-    render() {
-        return(
-            <div>Sign In Form</div>
-        )
-    }
-}
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      redirectTo: null
+    };
+  }
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSignIn = e => {
+    e.prevemtDefault();
+    console.log("handleSignIn");
 
+    axios
+      .post("/user/signin", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => {
+        console.log(res);
+        if (res.status === 200) {
+          this.props.updateUser({
+            loggedIn: true,
+            email: res.data.email
+          });
+          this.setState({ redirectTo: "/" });
+        }
+      })
+      .catch(error => {
+        console.log("login error: ");
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          >
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </input>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">
+              I agree to the Terms of Service and Privacy Policy.
+            </label>
+          </input>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Sign In
+        </button>
+      </form>
+    );
+  }
+}
 
 export default SignIn;
