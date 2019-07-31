1d310c85cde579c02c1b999e6b975e445d9981de
Nikki Daye <nikkidaye@gmail.com>
2019-07-31 12:58:06 -0400

    updated app.js and added content to components

---
+++ backend/database/models/user.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const bcrypt = require('bcryptjs');
+// mongoose.promise = Promise;
+
+// const userSchema = new Schema({
+//     email: { type: String },
+//     password: { type: String }
+// })
+
+// userSchema.methods = {
+//     c
+// }
