4cde611aaf34c0dad9f63b71239447036089393c
Kyesha Francis <franciskyesha1@gmail.com>
2019-08-01 14:51:57 -0400

    third push

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -42,7 +42,10 @@ class NewUser extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			firstName: ''
+			firstName: '',
+			lastName: '' ,
+			email: '',
+			password:'',
 		}
 	}
 
