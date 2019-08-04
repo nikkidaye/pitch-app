3d19ef0cc4c80feb1e65f4ca9df05924357e0af6
Cece Claridge <cece.claridge@gmail.com>
2019-08-04 09:22:22 -0400

    new user functioning

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const user = require("./routes/user");
-const port = process.env.Port || 5000;
-const session = require('express-session');
-const dbConnection = require('./database/index');
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const dbConnection = require('./database')
 const MongoStore = require('connect-mongo')(session)
-// const passport = require('./passport');
+const passport = require('./passport');
 const app = express()
-const mongoose = require('mongoose');
-const PORT = 8080;
-require('dotenv').config();
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:true }).then(
-  () => {
-    console.log("Connected to Mongo");
-  },
-  err => {
-    console.log("Error")
-  }
-)
-
-
+const PORT = 8080
+// Route requires
+const user = require('./routes/user')
 
 // MIDDLEWARE
 app.use(
@@ -33,18 +18,18 @@ app.use(
 app.use(bodyParser.json())
 
 // Sessions
-// app.use(
-// 	session({
-// 		secret: 'dream-team', //encrypts password
-// 		store: new MongoStore({ mongooseConnection: dbConnection }),
-// 		resave: false, //required
-// 		saveUninitialized: false //required
-// 	})
-// )
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
 
 // Passport
-// app.use(passport.initialize())
-// app.use(passport.session()) // calls the User
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
 
 
 // Routes
